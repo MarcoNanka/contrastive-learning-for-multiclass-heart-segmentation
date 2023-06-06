c05108aa22977cdcb557c69863d295e0a55fffe0
MarcoNanka <marco.nanka@tum.de>
2023-06-06 13:16:58 +0100

    implemented command-line tool

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+"""
+This code builds the command-line interface, which enables customisable model training.
+"""
+
 import argparse
+
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--n_of_slices_per_image', type=int, default=10, help='Radius of Cylinder')
+parser.add_argument('--n_of_slices_per_image', type=int, default=10,
+                    help='Determines number of layers of image')
+
+args: argparse.Namespace = parser.parse_args()
--- data_loading.py
+++ data_loading.py
@@ -1,23 +1,33 @@
+"""
+This code defines a class for the Multi-Modality Whole Heart Segmentation (MMWHS) dataset,
+which provides data loading functionality.
+
+def load_data - loads images into one 4D pytorch tensor (512: width x 512: height x
+n_of_slices_per_img: user input x total_number_of_slices/n_of_slices_per_img);
+"""
+
+
 import os
-from typing import Union, List, Tuple, Callable, Dict
+from typing import Union
 import numpy as np
 import torch
 import nibabel as nib
 import glob
 import math
+from config import parser, args
 
 
 class MMWHSDataset():
     def __init__(
             self,
             raw_data_dir: str, subfolders: Union[tuple, list, str],
-            n_of_slices_per_image
+            n_of_slices_per_img
     ) -> None:
         self.raw_data_dir = raw_data_dir
         self.subfolders = subfolders
-        self.data = self.load_data(n_of_slices_per_image)
+        self.data = self.load_data(n_of_slices_per_img)
 
-    def load_data(self, n_of_slices_per_image: int):
+    def load_data(self, n_of_slices_per_img: int):
         directory = self.raw_data_dir
         file_path_names = []
         for subfolder in self.subfolders:
@@ -29,8 +39,8 @@ class MMWHSDataset():
         for i, path in enumerate(file_path_names):
             buf = np.array(nib.load(path).get_fdata())
             n_of_slices = buf.shape[-1]
-            n_of_batches = math.ceil(n_of_slices / n_of_slices_per_image)
-            buf.resize([buf.shape[0], buf.shape[1], n_of_slices_per_image, n_of_batches])
+            n_of_batches = math.ceil(n_of_slices / n_of_slices_per_img)
+            buf.resize([buf.shape[0], buf.shape[1], n_of_slices_per_img, n_of_batches])
             for j in range(n_of_batches):
                 if i == 0 and j == 0:
                     img_data = np.array(buf[:, :, :, j])
@@ -43,4 +53,5 @@ class MMWHSDataset():
 
 if __name__ == "__main__":
     main_dir = "/Users/marconanka/BioMedia/data/even more reduced MM-WHS 2017 Dataset/"
-    dataset = MMWHSDataset(main_dir, ("ct_train", "ct_test"), 10)
+    dataset = MMWHSDataset(main_dir, ("ct_train", "ct_test"), args.n_of_slices_per_image)
+    print(dataset.data.shape)
