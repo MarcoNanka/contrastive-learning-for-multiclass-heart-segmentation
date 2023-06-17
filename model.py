ba3263f99086d503ed5842874186856a6054aef4
MarcoNanka <marco.nanka@tum.de>
2023-06-17 23:07:00 +0200

    small updates

--- model.py
+++ model.py
@@ -1,13 +1,10 @@
 from torch import nn
-import torch
 
 
 class BasicCNN(nn.Module):
     def __init__(self, num_classes):
         super(BasicCNN, self).__init__()
         self.features = nn.Sequential(
-            # in_channels: number of channels, for CT/MRI = 1, RGB = 3; out_channels: how many filters to apply
-            # both independent of image dimensions (width, height, depth)
             nn.Conv3d(1, 16, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool3d(kernel_size=2, stride=2),
@@ -15,13 +12,15 @@ class BasicCNN(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool3d(kernel_size=2, stride=2)
         )
-        self.classifier = nn.Linear(in_features=32 * 128 * 128 * 80, out_features=num_classes)
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=32 * 32 * 40 * 20, out_features=512),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=512, out_features=num_classes)
+        )
 
     def forward(self, x):
-        torch.set_default_tensor_type(torch.cuda.FloatTensor)
         x = self.features(x)
-        # changing shape of x to (batch_size, rest)
-        x = torch.flatten(x, start_dim=1, end_dim=-1)
+        x = x.view(x.size(0), -1)  # Reshape to (batch_size, num_features)
         x = self.classifier(x)
         out = {'out': x}
         return out
