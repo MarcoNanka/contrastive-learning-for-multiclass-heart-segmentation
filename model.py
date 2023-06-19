17d264b0a9db429dda21da8d414eef16891b62a5
MarcoNanka <marco.nanka@tum.de>
2023-06-19 10:02:39 +0200

    fixed input dimensions

--- model.py
+++ model.py
@@ -14,21 +14,14 @@ class BasicCNN(nn.Module):
             nn.MaxPool3d(kernel_size=3, stride=2)
         )
         self.classifier = nn.Sequential(
-            nn.Linear(in_features=self.calculate_num_features(), out_features=256),
+            nn.Linear(in_features=2*23225760, out_features=256),
             nn.ReLU(inplace=True),
             nn.Linear(in_features=256, out_features=num_classes)
         )
 
-    def calculate_num_features(self):
-        with torch.no_grad():
-            dummy_input = torch.zeros(1, 1, *self.input_size[2:])
-            features_output = self.features(dummy_input)
-        return features_output.view(dummy_input.size(0), -1).size(1)
-
     def forward(self, x):
-        self.input_size = x.size()
         x = self.features(x)
-        x = x.view(self.input_size[0], -1)
+        x = x.view(x.size(0), -1)
         x = self.classifier(x)
         out = {'out': x}
         return out
