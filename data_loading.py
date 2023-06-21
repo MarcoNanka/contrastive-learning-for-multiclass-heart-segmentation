1beb6332495244859aaf1153b332ac6e538752a2
MarcoNanka <marco.nanka@tum.de>
2023-06-21 16:50:19 +0200

    test

--- data_loading.py
+++ data_loading.py
@@ -103,10 +103,10 @@ class MMWHSDataset(Dataset):
         """
         label_values = np.sort(np.unique(raw_data))
         num_classes = len(label_values)
+        for ind, val in enumerate(label_values):
+            print(f"ind: {ind}, val: {val}")
+            raw_data[raw_data == val] = ind
 
-
-        raw_data = np.eye(num_classes)[raw_data.astype(int)]
-        raw_data = np.transpose(np.squeeze(raw_data), (0, 4, 1, 2, 3))
         return raw_data
 
     def create_training_data_array(self, path_list: list) -> np.ndarray:
