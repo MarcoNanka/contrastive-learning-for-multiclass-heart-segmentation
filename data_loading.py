dbf70a1a7e19bb430c3a745ed508d87cca8b5465
MarcoNanka <marco.nanka@tum.de>
2023-06-26 12:56:04 +0200

    exclude patches with no information to mitigate class imbalance

--- data_loading.py
+++ data_loading.py
@@ -45,17 +45,20 @@ class MMWHSDataset(Dataset):
        """
         return self.x[idx], self.y[idx]
 
-    def extract_patches(self, image_data: np.ndarray) -> np.ndarray:
+    def extract_patches(self, image_data: np.ndarray, label_data: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
         Extract patches from the given image data.
 
         Args:
             image_data (np.ndarray): The input image data.
+            label_data (np.ndarray): The input label data.
 
         Returns:
-            np.ndarray: An array of extracted patches.
+            np.ndarray: An array of extracted image patches.
+            np.ndarray: An array of extracted label patches.
         """
-        patches = []
+        image_patches = []
+        label_patches = []
         dim_x, dim_y, dim_z = image_data.shape
         mod_x = dim_x % self.patch_size[0]
         mod_y = dim_y % self.patch_size[1]
@@ -64,15 +67,22 @@ class MMWHSDataset(Dataset):
         pad_y = 0 if mod_y == 0 else self.patch_size[1] - mod_y
         pad_z = 0 if mod_z == 0 else self.patch_size[2] - mod_z
         image_data = np.pad(image_data, ((0, pad_x), (0, pad_y), (0, pad_z)), mode='constant')
+        label_data = np.pad(label_data, ((0, pad_x), (0, pad_y), (0, pad_z)), mode='constant')
 
         for x in range(0, image_data.shape[0], self.patch_size[0]):
             for y in range(0, image_data.shape[1], self.patch_size[1]):
                 for z in range(0, image_data.shape[2], self.patch_size[2]):
-                    patch = image_data[x: x + self.patch_size[0], y: y + self.patch_size[1], z: z + self.patch_size[2]]
-                    patch = np.expand_dims(patch, axis=0)
-                    patches.append(patch)
+                    img_patch = image_data[x: x + self.patch_size[0], y: y + self.patch_size[1],
+                                z: z + self.patch_size[2]]
+                    img_patch = np.expand_dims(img_patch, axis=0)
+                    label_patch = label_data[x: x + self.patch_size[0], y: y + self.patch_size[1],
+                                  z: z + self.patch_size[2]]
+                    label_patch = np.expand_dims(label_patch, axis=0)
+                    if len(np.unique(label_patch) > 1) or np.unique(label_patch)[0] != 0:
+                        image_patches.append(img_patch)
+                        label_patches.append(label_patch)
 
-        return np.array(patches)
+        return np.array(image_patches), np.array(label_patches)
 
     def normalize_minmax_data(self, raw_data: np.ndarray, min_val: float = 1, max_val: float = 99) -> np.ndarray:
         """
@@ -108,7 +118,7 @@ class MMWHSDataset(Dataset):
         raw_data = np.squeeze(raw_data)
         return raw_data, len(label_values)
 
-    def create_training_data_array(self, path_list: list) -> np.ndarray:
+    def create_training_data_array(self, path_list: list) -> Tuple[np.ndarray, np.ndarray]:
         """
         Create the training data array from the given list of paths.
 
@@ -116,16 +126,21 @@ class MMWHSDataset(Dataset):
             path_list (list): The list of file paths.
 
         Returns:
-            np.ndarray: The training data array.
+            np.ndarray: The patched image data.
+            np.ndarray: The patched label data.
         """
-        ret_array = []
+        patches_images = []
+        patches_labels = []
         for path in path_list:
-            array = np.array(nib.load(path).get_fdata())
-            array = self.extract_patches(array)
-            ret_array.append(array)
+            image_data = np.array(nib.load(path).get_fdata())
+            label_data = np.array(nib.load(path.replace('image', 'label')).get_fdata())
+            image_data, label_data = self.extract_patches(image_data, label_data)
+            patches_images.append(image_data)
+            patches_labels.append(label_data)
 
-        ret_array = np.concatenate(ret_array, axis=0)
-        return ret_array
+        patches_images = np.concatenate(patches_images, axis=0)
+        patches_labels = np.concatenate(patches_labels, axis=0)
+        return patches_images, patches_labels
 
     def get_training_data_from_system(self) -> Tuple[np.ndarray, np.ndarray]:
         """
@@ -137,9 +152,7 @@ class MMWHSDataset(Dataset):
         image_path_names = glob.glob(os.path.join(self.folder_path, "*image.nii*"))
         if not image_path_names:
             raise ValueError("Empty list! Check if folder path contains images.")
-        label_path_names = [path.replace('image', 'label') for path in image_path_names]
-        ret_imgs = self.create_training_data_array(image_path_names)
-        ret_labels = self.create_training_data_array(label_path_names)
+        ret_imgs, ret_labels = self.create_training_data_array(image_path_names)
         return ret_imgs, ret_labels
 
     def load_data(self) -> Tuple[torch.Tensor, torch.Tensor, int]:
@@ -162,6 +175,6 @@ if __name__ == "__main__":
     print(f"image data: {dataset.x.shape}")
     print(f"labels: {dataset.y.shape}")
     print(f"number of classes: {dataset.num_classes}")
-    print(f"number of channels: { dataset.x.shape[1]}")
+    print(f"number of channels: {dataset.x.shape[1]}")
     print(f"Image data: {dataset.x[6180:6200, 0, 12, 12, 12]}")
     print(f"corresponding labels: {dataset.y[6190:6200, 12, 12, 12]}")
