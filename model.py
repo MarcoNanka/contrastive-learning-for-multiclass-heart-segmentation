f802b31a4a7d83237219439e9eb4d7cb8d67e3dc
Marco Nanka <marconanka@Marcos-MacBook-Pro.local>
2023-09-13 21:46:30 +0200

    debugging model

--- model.py
+++ model.py
@@ -16,12 +16,15 @@ class UNet(nn.Module):
         # Contracting path: Increasing features, reducing spatial dimensions
         self.encoder_conv1 = nn.Conv3d(in_channels=in_channels, out_channels=16, kernel_size=3, padding=1)
         self.encoder_conv2 = nn.Conv3d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
-        self.pool1 = nn.MaxPool3d(kernel_size=2, stride=2)
+        self.encoder_conv3 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool3d(kernel_size=2, stride=2)
 
         # Expanding path: Decreasing features, increasing spatial dimensions
-        self.upconv1 = nn.ConvTranspose3d(in_channels=32, out_channels=16, kernel_size=2, stride=2)
-        self.decoder_conv1 = nn.Conv3d(in_channels=32, out_channels=16, kernel_size=3, padding=1)
-        self.decoder_conv2 = nn.Conv3d(in_channels=16, out_channels=num_classes, kernel_size=1)
+        self.upconv1 = nn.ConvTranspose3d(in_channels=64, out_channels=32, kernel_size=2, stride=2)
+        self.upconv2 = nn.ConvTranspose3d(in_channels=32, out_channels=16, kernel_size=2, stride=2)
+        self.decoder_conv1 = nn.Conv3d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
+        self.decoder_conv2 = nn.Conv3d(in_channels=32, out_channels=16, kernel_size=3, padding=1)
+        self.decoder_conv3 = nn.Conv3d(in_channels=16, out_channels=num_classes, kernel_size=1)
 
         self.relu = nn.ReLU()
 
@@ -36,24 +39,19 @@ class UNet(nn.Module):
            torch.Tensor: Output tensor of shape (batch_size, num_classes, depth, height, width).
        """
         # Contracting path: Finding High-level patterns
-        print("START")
-        print(x.shape)
-        x1 = self.relu(self.encoder_conv1(x))
-        print(x1.shape)
-        x2 = self.pool1(x1)
-        print(x2.shape)
-        x2 = self.relu(self.encoder_conv2(x2))
-        print(x2.shape)
+        x1 = self.relu(self.encoder_conv1(x))  # 16,16,24,24,24
+        x2 = self.pool(x1)  # 16,16,12,12,12
+        x2 = self.relu(self.encoder_conv2(x2))  # 16, 32, 12, 12, 12
+        x3 = self.pool(x2)  # 16, 32, 6, 6, 6
+        x3 = self.relu(self.encoder_conv3(x3))  # 16, 64, 6, 6, 6
 
         # Expanding path: Refining features
-        x3 = self.upconv1(x2)
-        print(x3.shape)
-        x3 = torch.cat((x1, x3), dim=1)
-        print(x3.shape)
-        x3 = self.relu(self.decoder_conv1(x3))
-        print(x3.shape)
-        output = self.decoder_conv2(x3)
-        print(output.shape)
-        print("END")
+        x4 = self.upconv1(x3)  # 16, 32, 12, 12, 12
+        x4 = torch.cat((x2, x4), dim=1)  # 16, 64, 12, 12, 12
+        x4 = self.relu(self.decoder_conv1(x4))  # 16, 32, 12, 12, 12
+        x5 = self.upconv2(x4)  # 16, 16, 24, 24, 24
+        x5 = torch.cat((x1, x5), dim=1)  # 16, 32, 24, 24, 24
+        x5 = self.relu(self.decoder_conv2(x5))  # 16, 16, 24, 24, 24
+        output = self.relu(self.decoder_conv3(x5))  # 16, 8, 24, 24, 24
 
         return output
