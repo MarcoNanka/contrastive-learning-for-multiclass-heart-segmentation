8a4cb08b9fb115ad887122d11c176101eb694371
Marco Nanka <marconanka@Marcos-MacBook-Pro.local>
2023-09-13 21:09:30 +0200

    debugging model

--- model.py
+++ model.py
@@ -16,14 +16,12 @@ class UNet(nn.Module):
         # Contracting path: Increasing features, reducing spatial dimensions
         self.encoder_conv1 = nn.Conv3d(in_channels=in_channels, out_channels=16, kernel_size=3, padding=1)
         self.encoder_conv2 = nn.Conv3d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
-        self.encoder_conv3 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool3d(kernel_size=2, stride=2)
 
         # Expanding path: Decreasing features, increasing spatial dimensions
-        self.upconv1 = nn.ConvTranspose3d(in_channels=64, out_channels=32, kernel_size=2, stride=2)
-        self.decoder_conv1 = nn.Conv3d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
-        self.decoder_conv2 = nn.Conv3d(in_channels=32, out_channels=16, kernel_size=3, padding=1)
-        self.final_conv = nn.Conv3d(in_channels=16, out_channels=num_classes, kernel_size=1)
+        self.upconv1 = nn.ConvTranspose3d(in_channels=32, out_channels=16, kernel_size=2, stride=2)
+        self.decoder_conv1 = nn.Conv3d(in_channels=32, out_channels=16, kernel_size=3, padding=1)
+        self.decoder_conv2 = nn.Conv3d(in_channels=16, out_channels=num_classes, kernel_size=1)
 
         self.relu = nn.ReLU()
 
@@ -39,15 +37,13 @@ class UNet(nn.Module):
        """
         # Contracting path: Finding High-level patterns
         x1 = self.relu(self.encoder_conv1(x))
-        x2 = self.relu(self.encoder_conv2(x1))
-        x3 = self.relu(self.encoder_conv3(x2))
-        x4 = self.relu(self.pool1(x3))
+        x2 = self.pool1(x1)
+        x2 = self.relu(self.encoder_conv2(x2))
 
         # Expanding path: Refining features
-        x5 = self.upconv1(x4)
-        x5 = self.relu(self.decoder_conv1(x5))
-        x5 = torch.cat((x3, x5), dim=1)
-        x6 = self.relu(self.decoder_conv2(x5))
-        output = self.final_conv(x6)
+        x3 = self.upconv1(x2)
+        x3 = torch.cat((x1, x3), dim=1)
+        x3 = self.relu(self.decoder_conv1(x3))
+        output = self.decoder_conv2(x3)
 
         return output
