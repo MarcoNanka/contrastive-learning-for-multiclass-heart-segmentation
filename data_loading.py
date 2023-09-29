7b8d52448529b7717fce0fe7f8d7a54a3680b584
Marco Nanka <marconanka@Marcos-MacBook-Pro.local>
2023-09-29 14:27:53 +0200

    extracted methods to DataProcessor class

--- data_loading.py
+++ data_loading.py
@@ -49,6 +49,109 @@ class DataProcessor:
         normalized_data = (raw_data - mean) / std_dev
         return normalized_data, mean, std_dev
 
+    @staticmethod
+    def get_training_data_from_system(folder_path: str, is_validation_dataset: bool, patch_size: Tuple[int, int, int],
+                                      patches_filter: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Load the training data from the file system.
+
+        Args:
+            folder_path (str)
+            is_validation_dataset (bool)
+            patch_size (tuple)
+            patches_filter (int)
+
+        Returns:
+            tuple: The input and target training data.
+        """
+        image_path_names = glob.glob(os.path.join(folder_path, "*image.nii*"))
+        if not image_path_names:
+            raise ValueError("Empty list! Check if folder path contains images.")
+        ret_imgs, ret_labels, original_image_data, original_label_data = \
+            DataProcessor.create_training_data_array(image_path_names, is_validation_dataset, patch_size,
+                                                     patches_filter)
+        return ret_imgs, ret_labels, original_image_data, original_label_data
+
+    @staticmethod
+    def create_training_data_array(path_list: list, is_validation_dataset: bool, patch_size: Tuple[int, int, int],
+                                   patches_filter: int) -> tuple[ndarray, ndarray, ndarray, ndarray]:
+        """
+        Create the training data array from the given list of paths.
+
+        Args:
+            path_list (list): The list of file paths.
+            is_validation_dataset (bool)
+            patch_size (tuple)
+            patches_filter (int)
+
+        Returns:
+            np.ndarray: The patched image data.
+            np.ndarray: The patched label data.
+        """
+        patches_images = []
+        patches_labels = []
+        for path in path_list:
+            image_data = np.array(nib.load(path).get_fdata())
+            label_data = np.array(nib.load(path.replace('image', 'label')).get_fdata())
+            original_image_data = image_data
+            original_label_data = label_data
+            image_data, label_data = DataProcessor.extract_patches(image_data, label_data, patch_size,
+                                                                   is_validation_dataset, patches_filter)
+            patches_images.append(image_data)
+            patches_labels.append(label_data)
+
+        patches_images = np.concatenate(patches_images, axis=0)
+        patches_labels = np.concatenate(patches_labels, axis=0)
+        print(f"is validation: {is_validation_dataset} -> shape of patches array: {patches_images.shape}")
+        return patches_images, patches_labels, original_image_data, original_label_data
+
+    @staticmethod
+    def extract_patches(image_data: np.ndarray, label_data: np.ndarray, patch_size: Tuple[int, int, int],
+                        is_validation_dataset: bool, patches_filter: int) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Extract patches from the given image data.
+
+        Args:
+            image_data (np.ndarray): The input image data, dimensions: (width, height, depth).
+            label_data (np.ndarray): The input label data.
+            patch_size (tuple)
+            is_validation_dataset (bool)
+            patches_filter (int)
+
+        Returns:
+            np.ndarray: An array of extracted image patches.
+            np.ndarray: An array of extracted label patches.
+        """
+        image_patches = []
+        label_patches = []
+        dim_x, dim_y, dim_z = image_data.shape
+        mod_x = dim_x % patch_size[0]
+        mod_y = dim_y % patch_size[1]
+        mod_z = dim_z % patch_size[2]
+        pad_x = 0 if mod_x == 0 else patch_size[0] - mod_x
+        pad_y = 0 if mod_y == 0 else patch_size[1] - mod_y
+        pad_z = 0 if mod_z == 0 else patch_size[2] - mod_z
+        image_data = np.pad(image_data, ((0, pad_x), (0, pad_y), (0, pad_z)), mode='constant')
+        label_data = np.pad(label_data, ((0, pad_x), (0, pad_y), (0, pad_z)), mode='constant')
+
+        for x in range(0, image_data.shape[0], patch_size[0]):
+            for y in range(0, image_data.shape[1], patch_size[1]):
+                for z in range(0, image_data.shape[2], patch_size[2]):
+                    img_patch = image_data[x: x + patch_size[0], y: y + patch_size[1],
+                                           z: z + patch_size[2]]
+                    img_patch = np.expand_dims(img_patch, axis=0)
+                    label_patch = label_data[x: x + patch_size[0], y: y + patch_size[1],
+                                             z: z + patch_size[2]]
+                    label_patch = np.expand_dims(label_patch, axis=0)
+                    unique, counts = np.unique(label_patch, return_counts=True)
+                    counts_descending = -np.sort(-counts)
+                    if is_validation_dataset or (len(unique) > 1 and counts_descending[1] >= patches_filter) \
+                            or unique[0] != 0:
+                        image_patches.append(img_patch)
+                        label_patches.append(label_patch)
+
+        return np.array(image_patches), np.array(label_patches)
+
 
 class MMWHSDataset(Dataset):
     """
@@ -99,89 +202,6 @@ class MMWHSDataset(Dataset):
        """
         return self.x[idx], self.y[idx]
 
-    def extract_patches(self, image_data: np.ndarray, label_data: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Extract patches from the given image data.
-
-        Args:
-            image_data (np.ndarray): The input image data, dimensions: (width, height, depth).
-            label_data (np.ndarray): The input label data.
-
-        Returns:
-            np.ndarray: An array of extracted image patches.
-            np.ndarray: An array of extracted label patches.
-        """
-        image_patches = []
-        label_patches = []
-        dim_x, dim_y, dim_z = image_data.shape
-        mod_x = dim_x % self.patch_size[0]
-        mod_y = dim_y % self.patch_size[1]
-        mod_z = dim_z % self.patch_size[2]
-        pad_x = 0 if mod_x == 0 else self.patch_size[0] - mod_x
-        pad_y = 0 if mod_y == 0 else self.patch_size[1] - mod_y
-        pad_z = 0 if mod_z == 0 else self.patch_size[2] - mod_z
-        image_data = np.pad(image_data, ((0, pad_x), (0, pad_y), (0, pad_z)), mode='constant')
-        label_data = np.pad(label_data, ((0, pad_x), (0, pad_y), (0, pad_z)), mode='constant')
-
-        for x in range(0, image_data.shape[0], self.patch_size[0]):
-            for y in range(0, image_data.shape[1], self.patch_size[1]):
-                for z in range(0, image_data.shape[2], self.patch_size[2]):
-                    img_patch = image_data[x: x + self.patch_size[0], y: y + self.patch_size[1],
-                                           z: z + self.patch_size[2]]
-                    img_patch = np.expand_dims(img_patch, axis=0)
-                    label_patch = label_data[x: x + self.patch_size[0], y: y + self.patch_size[1],
-                                             z: z + self.patch_size[2]]
-                    label_patch = np.expand_dims(label_patch, axis=0)
-                    unique, counts = np.unique(label_patch, return_counts=True)
-                    counts_descending = -np.sort(-counts)
-                    if self.is_validation_dataset or (len(unique) > 1 and counts_descending[1] >= self.patches_filter) \
-                            or unique[0] != 0:
-                        image_patches.append(img_patch)
-                        label_patches.append(label_patch)
-
-        return np.array(image_patches), np.array(label_patches)
-
-    def create_training_data_array(self, path_list: list) -> tuple[ndarray, ndarray, ndarray, ndarray]:
-        """
-        Create the training data array from the given list of paths.
-
-        Args:
-            path_list (list): The list of file paths.
-
-        Returns:
-            np.ndarray: The patched image data.
-            np.ndarray: The patched label data.
-        """
-        patches_images = []
-        patches_labels = []
-        for path in path_list:
-            image_data = np.array(nib.load(path).get_fdata())
-            label_data = np.array(nib.load(path.replace('image', 'label')).get_fdata())
-            original_image_data = image_data
-            original_label_data = label_data
-            image_data, label_data = self.extract_patches(image_data, label_data)
-            patches_images.append(image_data)
-            patches_labels.append(label_data)
-
-        patches_images = np.concatenate(patches_images, axis=0)
-        patches_labels = np.concatenate(patches_labels, axis=0)
-        print(f"is validation: {self.is_validation_dataset} -> shape of patches array: {patches_images.shape}")
-        return patches_images, patches_labels, original_image_data, original_label_data
-
-    def get_training_data_from_system(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-        """
-        Load the training data from the file system.
-
-        Returns:
-            tuple: The input and target training data.
-        """
-        image_path_names = glob.glob(os.path.join(self.folder_path, "*image.nii*"))
-        if not image_path_names:
-            raise ValueError("Empty list! Check if folder path contains images.")
-        ret_imgs, ret_labels, original_image_data, original_label_data = \
-            self.create_training_data_array(image_path_names)
-        return ret_imgs, ret_labels, original_image_data, original_label_data
-
     def load_data(self) -> Tuple[torch.Tensor, torch.Tensor, int, np.ndarray, np.ndarray, np.ndarray, float, float]:
         """
         Load and preprocess the dataset.
@@ -189,7 +209,9 @@ class MMWHSDataset(Dataset):
         Returns:
             tuple: The preprocessed input and target data tensors.
         """
-        img_data, label_data, original_image_data, original_label_data = self.get_training_data_from_system()
+        img_data, label_data, original_image_data, original_label_data = DataProcessor.\
+            get_training_data_from_system(self.folder_path, self.is_validation_dataset, self.patch_size,
+                                          self.patches_filter)
         img_data, mean, std_dev = DataProcessor.normalize_z_score_data(img_data, self.is_validation_dataset,
                                                                        self.mean, self.std_dev)
         label_data, num_classes, label_values = DataProcessor.preprocess_label_data(label_data)
