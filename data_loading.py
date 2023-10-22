9f860ba4b5e0695cbc4fa0e446d5e872ead8377e
Marco Nanka <marconanka@Marcos-MacBook-Pro.local>
2023-10-22 22:23:39 +0200

    domain contrastive loss: implemented extract patches

--- data_loading.py
+++ data_loading.py
@@ -277,12 +277,12 @@ class MMWHSContrastiveDataset(Dataset):
                                           image_type=self.image_type)
         if self.image_type == "MRI":
             img_data = [arr for arr in img_data if (arr.shape[3] == 512 and arr.shape[4] >= 120)]
-            for arr in img_data:
+            for idx, arr in enumerate(img_data):
                 target_length = 120
                 remove_from_start = int((arr.shape[4] - target_length) / 2)
                 remove_from_end = int(arr.shape[4] - target_length - remove_from_start)
-                arr = arr[:, :, :, :, remove_from_start:arr.shape[4] - remove_from_end]
-                print(arr.shape)
+                img_data[idx] = arr[:, :, :, :, remove_from_start:arr.shape[4] - remove_from_end]
+                print(img_data[idx].shape)
         for i in img_data:
             print(f"SHAPE: {i.shape}")
         img_data = np.concatenate(img_data, axis=0)
