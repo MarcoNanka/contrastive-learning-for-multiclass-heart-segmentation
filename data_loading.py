94f78a26b79e05b3ff78441ce9bb0981f4d65070
Marco Nanka <marconanka@Marcos-MacBook-Pro.local>
2023-10-24 21:52:38 +0200

    wrote data loading more compact

--- data_loading.py
+++ data_loading.py
@@ -12,13 +12,8 @@ from monai.transforms import Compose, RandFlip, ToTensor, RandGaussianNoise, Ran
 class DataProcessor:
     @staticmethod
     def preprocess_label_data(raw_data: np.ndarray) -> Tuple[np.ndarray, int, np.ndarray]:
-        """
-        Prepare the label data for training.
-        """
         label_values = np.array([0., 205., 420., 500., 550., 600., 820., 850.])
-        for ind, val in enumerate(label_values):
-            raw_data[raw_data == val] = ind
-
+        raw_data = np.digitize(raw_data, label_values) - 1
         raw_data = np.squeeze(raw_data)
         return raw_data, len(label_values), label_values
 
@@ -29,9 +24,7 @@ class DataProcessor:
         Normalize the given raw data using z-score normalization.
         """
         if not is_validation_dataset:
-            mean = float(np.mean(raw_data))
-            std_dev = float(np.std(raw_data))
-
+            mean, std_dev = float(np.mean(raw_data)), float(np.std(raw_data))
         normalized_data = (raw_data - mean) / std_dev
         return normalized_data, mean, std_dev
 
@@ -42,18 +35,16 @@ class DataProcessor:
         Reconstruct the original label data from extracted label patches of the validation dataset.
         """
         original_shape = original_label_data.shape
-        dim_x, dim_y, dim_z = original_shape
         label_data = np.zeros(original_shape, dtype=label_patches.dtype)
-
         patch_index = 0
 
-        for x in range(0, dim_x, patch_size[0]):
-            for y in range(0, dim_y, patch_size[1]):
-                for z in range(0, dim_z, patch_size[2]):
+        for x in range(0, original_shape[0], patch_size[0]):
+            for y in range(0, original_shape[1], patch_size[1]):
+                for z in range(0, original_shape[2], patch_size[2]):
                     label_patch = label_patches[patch_index]
-                    x_end = min(x + patch_size[0], dim_x)
-                    y_end = min(y + patch_size[1], dim_y)
-                    z_end = min(z + patch_size[2], dim_z)
+                    x_end = min(x + patch_size[0], original_shape[0])
+                    y_end = min(y + patch_size[1], original_shape[1])
+                    z_end = min(z + patch_size[2], original_shape[2])
                     label_data[x:x_end, y:y_end, z:z_end] = label_patch[:x_end - x, :y_end - y, :z_end - z]
                     patch_index += 1
 
@@ -70,19 +61,13 @@ class DataProcessor:
             image_patches = []
             label_patches = []
             dim_x, dim_y, dim_z = image_data.shape
-            mod_x = dim_x % patch_size[0]
-            mod_y = dim_y % patch_size[1]
-            mod_z = dim_z % patch_size[2]
+            mod_x, mod_y, mod_z = dim_x % patch_size[0], dim_y % patch_size[1], dim_z % patch_size[2]
             pad_x = 0 if mod_x == 0 else patch_size[0] - mod_x
             pad_y = 0 if mod_y == 0 else patch_size[1] - mod_y
             pad_z = 0 if mod_z == 0 else patch_size[2] - mod_z
             label_data = np.pad(label_data, ((0, pad_x), (0, pad_y), (0, pad_z)), mode='constant', constant_values=0)
-            if image_type == "MRI":
-                image_data = np.pad(image_data, ((0, pad_x), (0, pad_y), (0, pad_z)), mode='constant',
-                                    constant_values=0)
-            else:
-                image_data = np.pad(image_data, ((0, pad_x), (0, pad_y), (0, pad_z)), mode='constant',
-                                    constant_values=-3022)
+            image_data = np.pad(image_data, ((0, pad_x), (0, pad_y), (0, pad_z)), mode='constant',
+                                constant_values=(0 if image_type == "MRI" else -3022))
 
             for x in range(0, image_data.shape[0], patch_size[0]):
                 for y in range(0, image_data.shape[1], patch_size[1]):
@@ -106,18 +91,12 @@ class DataProcessor:
         else:
             image_patches = []
             label_patches = []
-            # CT: (right -> left, posterior -> anterior, inferior -> superior)
-            # MRI: (anterior -> posterior, inferior -> superior, left -> right)
-            posterior_anterior = image_data.shape[0] if image_type == "MRI" else image_data.shape[1]
-            remainder = posterior_anterior % patch_size[2]
+            posterior_anterior_axis = image_data.shape[0] if image_type == "MRI" else image_data.shape[1]
+            remainder = posterior_anterior_axis % patch_size[2]
 
-            for i in range(0 + remainder // 2, posterior_anterior - (remainder + 1) // 2, patch_size[2]):
-                if image_type == "MRI" and image_data.shape[1] == 512:
-                    img_patch = image_data[i: i + patch_size[2], :, :]
-                elif image_type == "CT":
-                    img_patch = image_data[:, i: i + patch_size[2], :]
-                else:
-                    img_patch = np.zeros((1, 1, 1))
+            for i in range(remainder // 2, posterior_anterior_axis - (remainder + 1) // 2, patch_size[2]):
+                img_patch = (image_data[i:i + patch_size[2], :, :] if image_type == "MRI" and image_data.shape[1] == 512
+                             else image_data[:, i:i + patch_size[2], :])
                 img_patch = np.expand_dims(img_patch, axis=0)
                 label_patch = np.empty((0, 0, 0))
                 image_patches.append(img_patch)
@@ -137,11 +116,9 @@ class DataProcessor:
         for path in path_list:
             image_data = np.array(nib.load(path).get_fdata())
             original_image_data = image_data
-            if not is_contrastive_dataset:
-                label_data = np.array(nib.load(path.replace('image', 'label')).get_fdata())
-                original_label_data = label_data
-            else:
-                label_data = original_label_data = np.empty((0, 0, 0))
+            original_label_data = np.array(nib.load(path.replace('image', 'label')).get_fdata()) \
+                if not is_contrastive_dataset else np.empty((0, 0, 0))
+            label_data = original_label_data.copy() if not is_contrastive_dataset else original_label_data
             image_data, label_data = DataProcessor.extract_patches(image_data=image_data, label_data=label_data,
                                                                    patch_size=patch_size,
                                                                    is_validation_dataset=is_validation_dataset,
@@ -212,8 +189,7 @@ class MMWHSDataset(Dataset):
             get_training_data_from_system(folder_path=self.folder_path,
                                           is_validation_dataset=self.is_validation_dataset, patch_size=self.patch_size,
                                           patches_filter=self.patches_filter, image_type=self.image_type)
-        img_data = np.concatenate(img_data, axis=0)
-        label_data = np.concatenate(label_data, axis=0)
+        img_data, label_data = np.concatenate(img_data, axis=0), np.concatenate(label_data, axis=0)
         print(f"is validation: {self.is_validation_dataset} -> shape of patches array: {img_data.shape}")
         img_data, mean, std_dev = DataProcessor.normalize_z_score_data(raw_data=img_data, is_validation_dataset=self.
                                                                        is_validation_dataset, mean=self.mean,
@@ -337,12 +313,10 @@ class MMWHSDomainContrastiveDataset(Dataset):
             return pair, distance
 
         else:
-            # POSITIVE PAIR
             positive_pair = self.transform(self.x[idx]), \
                 self.transform(self.x[idx_position + self.num_of_partitions * rand_other_img_idx])
             positive_label = torch.tensor(1.0)
 
-            # NEGATIVE PAIR
             negative_idx_position = torch.randint(0, self.num_of_partitions, (1,)).item()
             while abs(negative_idx_position - idx_position) <= 25:
                 negative_idx_position = torch.randint(0, self.num_of_partitions, (1,)).item()
