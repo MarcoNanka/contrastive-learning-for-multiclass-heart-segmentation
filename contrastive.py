e6d25702062b0eb3587c24f3961f6fb9bfcd37f0
Marco Nanka <marconanka@Marcos-MacBook-Pro.local>
2023-10-24 10:34:20 +0200

    implement loss for distance-adjusted domain contrastive loss

--- contrastive.py
+++ contrastive.py
@@ -63,8 +63,7 @@ class DistanceAdjustedContrastiveLoss(nn.Module):
         # Apply weights to the similarities
         print(f"similarities.shape: {similarities.shape}, weights.shape: {weights.shape}")
         weighted_similarity = torch.zeros_like(similarities)
-        for idx, _ in enumerate(weights):
-            weighted_similarity[idx] = similarities[idx] * weights[idx]
+        weighted_similarity = similarities * weights
         print(f"weighted_similarity: {weighted_similarity.shape}")
 
         # Calculate the loss as the mean squared error between weighted similarity and 1 (max similarity)
