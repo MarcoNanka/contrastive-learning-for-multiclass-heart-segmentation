e071971ade6010d2a351b71b054a37cc350e5158
Marco Nanka <marconanka@Marcos-MacBook-Pro.local>
2023-12-15 12:02:36 +0100

    replaced results table, now based on test set (correct)

--- results.py
+++ results.py
@@ -2,11 +2,11 @@ import numpy as np
 
 # Values from the table
 results = np.array([
-    [0.439, 0.506, 0.87, 0.317, 0.388, 0.698],
-    [0.502, 0.59, 0.878, 0.322, 0.448, 0.795],
-    [0.531, 0.62, 0.895, 0.345, 0.451, 0.795],
-    [0.49, 0.61, 0.909, 0.358, 0.435, 0.761],
-    [0.495, 0.608, 0.918, 0.368, 0.477, 0.77]
+    [0.362, 0.464, 0.734, 0.226, 0.285, 0.395],
+    [0.477, 0.5, 0.758, 0.269, 0.319, 0.439],
+    [0.492, 0.565, 0.761, 0.298, 0.329, 0.434],
+    [0.45, 0.523, 0.752, 0.285, 0.354, 0.445],
+    [0.543, 0.574, 0.741, 0.3, 0.351, 0.455]
 ])
 
 ct_mean = np.sum(results[:, :3])/15
@@ -25,4 +25,4 @@ global_threshold_mean = np.sum(results[1, :])/6
 random_mean = np.sum(results[0, :])/6
 not_random_mean = np.sum(results[1:, :])/24
 
-print(combined_mean, local_mean, combined_mean/local_mean)
+print(ct_1_mean, ct_2_mean, ct_8_mean)
